9e27abbe28baefecb417dc1e8d16b78c7d93fc0e
RodrigoSpano <rorrospano17@gmail.com>
2023-07-05 11:27:07 -0300

    post validation

--- api/src/controllers/pokemon/postPokemon.js
+++ api/src/controllers/pokemon/postPokemon.js
@@ -4,7 +4,7 @@ const postPokemon = async (req, res) => {
   try {
     const { name, hp, attack, defense, speed, height, weight, image, type } = req.body
     const findPokemon = await Pokemon.findOne({ where: { name: name.toLowerCase() } })
-    if (findPokemon) throw new Error('Cannot create an existing Pokemon')
+    if (findPokemon) return res.status(302).json({ message: 'this pokemon already exists' })
     const pokemonObj = { name: name.toLowerCase(), hp, attack, defense, speed, height, weight, image: image ? image : 'https://i.postimg.cc/Qdwz52bZ/defaultimage-1.jpg' }
     const newPokemon = await Pokemon.create(pokemonObj)
     newPokemon.addTypes(type) //tiene q ser el id del type 
--- api/src/middleware/validatePokemon.js
+++ api/src/middleware/validatePokemon.js
@@ -1,8 +1,14 @@
 const { Pokemon } = require('../db')
+const axios = require('axios')
 
-const validatePokemonMiddleware = (req, res, next) => {
+const pokeApi = 'https://pokeapi.co/api/v2'
+
+const validatePokemonMiddleware = async (req, res, next) => {
   const { name, hp, attack, defense, speed, height, weight, image, type } = req.body
   if (!name || !hp || !attack || !defense || !speed || !height || !weight || !image || !type) return res.status(400).json({ message: 'Missing values' })
+  const { data } = await axios(`${pokeApi}/pokemon/?limit=48`)
+  const findName = data.results.some(el => el.name.toLowerCase() === name.toLowerCase())
+  if (findName) return res.status(302).json({ message: 'this pokemon already exists' })
   next()
 }
 
