f61ddebb7368316c440d5674e26e8c56bfbe1eef
RodrigoSpano <rorrospano17@gmail.com>
2023-07-05 22:53:30 -0300

    getAll pokemons optimized

--- api/src/handlers/getPokemonsHandler.js
+++ api/src/handlers/getPokemonsHandler.js
@@ -1,30 +1,54 @@
 const { Pokemon, Type } = require('../db')
 const axios = require('axios')
 
+const getEachPokemonData = async (pokemons) => {
+  try {
+    const pokemonPromises = pokemons.map(async (poke) => {
+      const { data } = await axios(poke.url);
+      return {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.other['official-artwork'].front_default,
+        hp: data.stats.find(stat => stat.stat.name === 'hp')?.base_stat,
+        attack: data.stats.find(stat => stat.stat.name === 'attack')?.base_stat,
+        defense: data.stats.find(stat => stat.stat.name === 'defense')?.base_stat,
+        speed: data.stats.find(stat => stat.stat.name === 'speed')?.base_stat,
+        height: data.height,
+        weight: data.weight,
+        types: data.types,
+      };
+    });
+    return Promise.all(pokemonPromises);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getApiPokemons = async (URI) => {
+  try {
+    const { data } = await axios(`${URI}/pokemon/?limit=36`)
+    const pokemons = await getEachPokemonData(data.results)
+    if (pokemons) return pokemons
+  } catch (error) {
+    throw Error(error.message)
+  }
+}
+
+const getDbPokemons = async () => {
+  try {
+    return await Pokemon.findAll({ include: { model: Type, attributes: ['name'], through: { attributes: [] } } })
+  } catch (error) {
+    throw Error(error.message)
+  }
+}
 
 const getAllPokemons = async (URI) => {
   try {
     // pokemons from api
-    const pokemonsArray = []
-    const resp = await axios(`${URI}/pokemon/?limit=48`)
-    for (let poke of resp.data.results) {
-      const { data } = await axios(poke.url)
-      pokemonsArray.push({
-        id: data.id,
-        name: data.name,
-        image: data.sprites.other['official-artwork'].front_default,
-        hp: data.stats[0].stat.name === 'hp' && data.stats[0].base_stat,
-        attack: data.stats[1].stat.name === 'attack' && data.stats[1].base_stat,
-        defense: data.stats[2].stat.name === 'defense' && data.stats[2].base_stat,
-        speed: data.stats[5].stat.name === 'speed' && data.stats[5].base_stat,
-        height: data.height,
-        weight: data.weight,
-        types: data.types
-      })
-    }
+    const pokemonsApi = await getApiPokemons(URI)
     //pokemons from DB
-    const dbPokemons = await Pokemon.findAll({ include: { model: Type, attributes: ['name'], through: { attributes: [] } } })
-    return { DB_pokemons: dbPokemons.length ? dbPokemons : [], API_pokemons: pokemonsArray }
+    const pokemonsDb = await getDbPokemons()
+    return [...pokemonsDb, ...pokemonsApi]
   } catch (error) {
     throw Error(error)
   }
