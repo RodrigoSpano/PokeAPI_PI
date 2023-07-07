c0a136b1becfda1394b61dc250e5a490a700e4d3
RodrigoSpano <rorrospano17@gmail.com>
2023-07-07 10:38:07 -0300

    pagination style

--- client/src/components/homeComponents/subComps/pokemon/Pagination.jsx
+++ client/src/components/homeComponents/subComps/pokemon/Pagination.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
+import { useSelector } from "react-redux";
 import styles from "./pokemon.module.css";
 
 const Pagination = ({ prevFn, nextFn, pages }) => {
+  const pokemons = useSelector((state) => state.pokemons);
+  let disabledStyle = pokemons.length ? null : styles.disabledPag;
   return (
-    <div className={styles.paginationCont}>
+    <div className={`${styles.paginationCont} ${disabledStyle}`}>
       <button id={styles.prev} onClick={prevFn}>
         arrow
       </button>
