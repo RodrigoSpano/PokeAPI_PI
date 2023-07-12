2253b64140a09e8b82041d70001a24527514ad75
RodrigoSpano <rorrospano17@gmail.com>
2023-07-12 19:52:20 -0300

    create pokemon

--- api/src/controllers/pokemon/postPokemon.js
+++ api/src/controllers/pokemon/postPokemon.js
@@ -2,12 +2,12 @@ const { Pokemon } = require('../../db')
 
 const postPokemon = async (req, res) => {
   try {
-    const { name, hp, attack, defense, speed, height, weight, image, type } = req.body
+    const { name, hp, attack, defense, speed, height, weight, image, types } = req.body
     const findPokemon = await Pokemon.findOne({ where: { name: name.toLowerCase() } })
     if (findPokemon) return res.status(302).json({ message: 'this pokemon already exists' })
     const pokemonObj = { name: name.toLowerCase(), hp, attack, defense, speed, height, weight, image: image ? image : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/123.png' }
     const newPokemon = await Pokemon.create(pokemonObj)
-    newPokemon.addTypes(type) //tiene q ser el id del type 
+    newPokemon.addTypes(types) //tiene q ser el id del type 
     return res.status(201).json(newPokemon)
   } catch (error) {
     return res.status(500).json({ error: error.message })
