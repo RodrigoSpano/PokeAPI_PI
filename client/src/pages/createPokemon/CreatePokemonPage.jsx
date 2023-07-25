efb341788e49d85bf606d86ea2eaf3dd45a1f4e0
RodrigoSpano <rorrospano17@gmail.com>
2023-07-25 18:01:43 -0300

    fix: bug in form

--- client/src/pages/createPokemon/CreatePokemonPage.jsx
+++ client/src/pages/createPokemon/CreatePokemonPage.jsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import styles from "./createpokemons.module.css";
 import { useDispatch } from "react-redux";
 import { getTypesAction } from "../../redux/pokemons/action";
-import { CreatePokemon } from "../../components/createPokemon/createForm";
+import { CreatePokemon } from "../../components/createPokemon/createForm.jsx";
 
 const CreatePokemonPage = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTypesAction());
-  }, []);
+  }, [dispatch]);
   return (
     <div className={styles.pageCont}>
       <CreatePokemon />
